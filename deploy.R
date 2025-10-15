e4edaaba806a6bf81878faf17a400699ba0d4eab
jciturras <jciturra@uc.cl>
2025-10-15 10:59:01 +0200

    orden archivos
    - todo se fue a papelera: respaldo de códigos antiguos
    - archivo deploy actualizado

--- deploy.R
+++ deploy.R
@@ -2,6 +2,12 @@ library(pacman)
 p_load(rsconnect,
        quarto)
 
-rsconnect::setAccountInfo(name='gabrielcortesp', token='8ED1A66A016F143C75BD71D230D3C0A2', secret='sQ0pJkQZepkHpzWbjLM/d3oVh9QDN2r1wET6ry+l')
 
-quarto_publish_app(server="shinyapps.io")
+rsconnect::setAccountInfo(name='ocs-coes',
+                          token='966FC8FD0EF43CB5B3EFB982C32530A7',
+                          secret='gGXd5aw/yza5GHnyukmmeoMjicFUfeqWLgyP056c')
+
+
+quarto_publish_app(input = "viselsoc.qmd",
+                   server="shinyapps.io",
+                   name = "ocsvis-elsoc",account = "ocs-coes")
